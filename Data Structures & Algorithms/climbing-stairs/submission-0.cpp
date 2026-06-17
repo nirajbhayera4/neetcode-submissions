8807b1df98f29c44a00924e8a33547e352a296d3
NIRAJ <nirajbhayera4@gmail.com>
2026-06-17 20:21:02 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==1 || n==2 || n==3){
+            return n;
+        } 
+
+        int a=1;
+        int b=2;
+        int c=3;
+
+        for(int i=3;i<=n;i++){
+            c= b+ a;
+
+            int temp=b;
+            b=c;
+            a=temp;
+        }
+        return c;
+    }
+};
